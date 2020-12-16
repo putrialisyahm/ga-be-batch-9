18a4381dad4a7200fb4f997b03ed361fda241a24
salamidrus <idrussalam95gmail.com>
2020-12-16 21:03:55 +0700

    Add Data Filter 3

---
+++ challenges/dataFilter-2.js
@@ -0,0 +1,240 @@
+let data = [
+  {
+    profilePhoto:
+      "https://res.cloudinary.com/di02ey9t7/image/upload/v1602390817/kxnw44tsirdxfg8jafkv.jpg",
+    posts: [],
+    likes: [],
+    meetings: [],
+    _id: "5f7f3818fd6f3fd3b5b24dbc",
+    email: "idrus@gmail.com",
+    password: "$2b$10$ac33IXGYKSE0iz8aBzmfNOqZzvMQkQThIlnADCza1oQRpzAKKg0Ii",
+    __v: 0,
+    fullName: "M Idrus Salam",
+    mobileNumber: "08543891",
+    updatedAt: "2020-11-25T15:02:04.520Z",
+  },
+  {
+    profilePhoto:
+      "https://res.cloudinary.com/drovood07/image/upload/v1574847777/uploads/doctor_xp3dmi.png",
+    posts: ["5fbe6e6b9b10800017d2823a", "5fbe71a39b10800017d2825e"],
+    likes: ["5f9507ea39e07f1ef2fc49b4"],
+    meetings: [],
+    _id: "5f828c43c632cc2d1fb94605",
+    email: "idrus1@gmail.com",
+    password: "$2b$10$QKxIaD7r4mlQYIpAYOvtRO0v49SLjUrDsUH/IXNAyVwP75FmaouKW",
+    __v: 0,
+    updatedAt: "2020-11-25T15:02:03.969Z",
+  },
+  {
+    profilePhoto:
+      "https://res.cloudinary.com/drovood07/image/upload/v1574847777/uploads/doctor_xp3dmi.png",
+    posts: [],
+    likes: [],
+    meetings: [],
+    _id: "5f828e26dd9f632fa817f0a2",
+    email: "idrus3@gmail.com",
+    password: "$2b$10$I3ymp6j8bThB0E84zir.8OMripD4M4lnC7emYzODyTbNzBqEIGs1C",
+    __v: 0,
+  },
+  {
+    profilePhoto:
+      "https://res.cloudinary.com/drovood07/image/upload/v1574847777/uploads/doctor_xp3dmi.png",
+    posts: [
+      "5fbe70e29b10800017d28254",
+      "5fbe72609b10800017d28267",
+      "5fbe756a9b10800017d282bb",
+      "5fbe77a19b10800017d282e0",
+      "5fbe786a9b10800017d282ec",
+      "5fbe78b29b10800017d282f1",
+      "5fbe78e59b10800017d282f7",
+      "5fbe7bfb9b10800017d2831e",
+      "5fbe7c339b10800017d28323",
+    ],
+    likes: [],
+    meetings: [],
+    _id: "5f8d91d9e600870017e1b0e3",
+    email: "afdallah.war@gmail.com",
+    password: "$2b$10$uNa9JT5bPdA9dZF.SwKPxeZLJY.kZHn.p5lzhN2C58rZ49Xsqn1tW",
+    createdAt: "2020-10-19T13:17:13.468Z",
+    updatedAt: "2020-11-25T15:45:55.038Z",
+    __v: 0,
+  },
+  {
+    profilePhoto:
+      "https://res.cloudinary.com/drovood07/image/upload/v1574847777/uploads/doctor_xp3dmi.png",
+    posts: [],
+    likes: [],
+    meetings: [],
+    _id: "5f8d92a9e600870017e1b0e4",
+    email: "herman1@gmail.com",
+    password: "$2b$10$mYgkJmH2EVfGb6w.PFE/sOo2cR8Rt8hMxThyFykUW09HpvTa69pia",
+    createdAt: "2020-10-19T13:20:41.875Z",
+    updatedAt: "2020-10-19T13:20:41.875Z",
+    __v: 0,
+  },
+  {
+    profilePhoto:
+      "https://res.cloudinary.com/drovood07/image/upload/v1574847777/uploads/doctor_xp3dmi.png",
+    posts: [],
+    likes: [],
+    meetings: [],
+    _id: "5f8d934de600870017e1b0e5",
+    email: "wijaya@gmail.com",
+    password: "$2b$10$90itEDZbxAKw3CASNvJrdOS8fEu6Ip0EkiCvsxN73YQabzwondKZK",
+    createdAt: "2020-10-19T13:23:25.412Z",
+    updatedAt: "2020-10-19T13:23:25.412Z",
+    __v: 0,
+  },
+  {
+    profilePhoto:
+      "https://res.cloudinary.com/drovood07/image/upload/v1574847777/uploads/doctor_xp3dmi.png",
+    posts: [],
+    likes: [],
+    meetings: [],
+    _id: "5f8d952de600870017e1b0e6",
+    email: "mamat@gmail.com",
+    password: "$2b$10$ZPk8HJLobd/CRrzo3YQ5xOh2KLucqT9ir3.X02ORizQ9Z/IOBP9Ru",
+    createdAt: "2020-10-19T13:31:25.415Z",
+    updatedAt: "2020-10-19T13:31:25.415Z",
+    __v: 0,
+  },
+  {
+    profilePhoto:
+      "https://res.cloudinary.com/drovood07/image/upload/v1574847777/uploads/doctor_xp3dmi.png",
+    posts: [],
+    likes: [],
+    meetings: [],
+    _id: "5f8da7d9094d0f0017b7999e",
+    email: "rizal@gmail.com",
+    password: "$2b$10$tS8IRpbnYYX8Rh1uLki2bOcqrZm6plqon534vQW4xI18wWrLq.A7a",
+    createdAt: "2020-10-19T14:51:05.666Z",
+    updatedAt: "2020-10-19T14:51:05.666Z",
+    __v: 0,
+  },
+  {
+    profilePhoto:
+      "https://res.cloudinary.com/drovood07/image/upload/v1574847777/uploads/doctor_xp3dmi.png",
+    posts: [],
+    likes: [],
+    meetings: [],
+    _id: "5f8f061794f65f110af935f6",
+    email: "idrus2@gmail.com",
+    password: "$2b$10$UU9u0BLit44AoKo1.oiKIOGdmqh6TsmKtZlIAyPAqF438ACiM1V3O",
+    createdAt: "2020-10-20T15:45:28.033Z",
+    updatedAt: "2020-10-20T15:45:28.033Z",
+    __v: 0,
+  },
+  {
+    profilePhoto:
+      "https://res.cloudinary.com/drovood07/image/upload/v1574847777/uploads/doctor_xp3dmi.png",
+    posts: [],
+    likes: [],
+    meetings: [],
+    _id: "5f8f08aae0a4bd001791c2a2",
+    email: "idrus4@gmail.com",
+    password: "$2b$10$nnGBupuJ2qVfbxOZTtWzbuC8ZvBNrdSHH4Vpo/oQk2LO/pqERBinq",
+    createdAt: "2020-10-20T15:56:26.615Z",
+    updatedAt: "2020-10-20T15:56:26.615Z",
+    __v: 0,
+  },
+  {
+    profilePhoto:
+      "https://res.cloudinary.com/drovood07/image/upload/v1574847777/uploads/doctor_xp3dmi.png",
+    posts: [],
+    likes: [],
+    meetings: [],
+    _id: "5f94f9c6a0e53900170458a1",
+    email: "afdallah.wr@gmail.com",
+    password: "$2b$10$GxF6OJUyDwDu3hnnKzh4YuSHTVn9GPPPXAei17XcECZupAB6cbpj.",
+    createdAt: "2020-10-25T04:06:30.965Z",
+    updatedAt: "2020-10-25T04:06:30.965Z",
+    __v: 0,
+  },
+  {
+    profilePhoto:
+      "https://res.cloudinary.com/drovood07/image/upload/v1574847777/uploads/doctor_xp3dmi.png",
+    posts: [],
+    likes: [],
+    meetings: [],
+    _id: "5f958005b96c080017b4746d",
+    email: "ibnusina@gmail.com",
+    password: "$2b$10$BRebvW4xfnG7Hk0bfOOWo.GsEO3F1wUuro/DvaIU2V2AthqjHhgy2",
+    fullName: "Ibnu sina",
+    createdAt: "2020-10-25T13:39:17.112Z",
+    updatedAt: "2020-10-25T13:39:17.112Z",
+    __v: 0,
+  },
+  {
+    profilePhoto:
+      "https://res.cloudinary.com/drovood07/image/upload/v1574847777/uploads/doctor_xp3dmi.png",
+    posts: [],
+    likes: [],
+    meetings: [],
+    _id: "5fb66a365132d40017c2e1c8",
+    email: "nana@gmail.com",
+    password: "$2b$10$9XXEyoI.nUeEwwqnFX4QbuV.9JJycXZfkXUTV2k9Y37BMa2UMisva",
+    fullName: "nana",
+    createdAt: "2020-11-19T12:51:02.577Z",
+    updatedAt: "2020-11-19T12:51:02.577Z",
+    __v: 0,
+  },
+  {
+    profilePhoto:
+      "https://res.cloudinary.com/drovood07/image/upload/v1574847777/uploads/doctor_xp3dmi.png",
+    posts: [],
+    likes: [],
+    meetings: [],
+    _id: "5fbe841f9b10800017d28382",
+    email: "soimah@gmail.com",
+    password: "$2b$10$15MvyR0oeAvIj0zefMi2kOTqDvzLGKJVCPSv8.JJG4Yjzd1EEjEba",
+    fullName: "soimah",
+    createdAt: "2020-11-25T16:19:43.736Z",
+    updatedAt: "2020-11-25T16:19:43.736Z",
+    __v: 0,
+  },
+  {
+    profilePhoto:
+      "https://res.cloudinary.com/drovood07/image/upload/v1574847777/uploads/doctor_xp3dmi.png",
+    posts: ["5fd07aa2f832d00017396330"],
+    likes: [],
+    meetings: [],
+    _id: "5fd07a4af832d00017396329",
+    email: "2pl@bk.pl",
+    password: "$2b$10$NHgathfAvpcPwO7Uo1/5.uUyWwb.2utFvuADx.M9kxbJXUY/x1zqy",
+    fullName: "Name",
+    createdAt: "2020-12-09T07:18:34.490Z",
+    updatedAt: "2020-12-09T07:20:02.737Z",
+    __v: 0,
+  },
+];
+
+// filter, user yang punya mobileNumber
+// for konvensional
+let arr = [];
+for (let i = 0; i < data.length; i++) {
+  if (data[i].mobileNumber) {
+    arr.push(data[i]);
+  }
+}
+// console.log(arr);
+// for of
+let arrForOf = [];
+for (let item of data) {
+  if (item.mobileNumber) arrForOf.push(item);
+}
+
+// console.log(arrForOf);
+
+// filter
+let arrFilter = data.filter((item) => item.mobileNumber);
+// console.log(arrFilter);
+
+// filter, user yang punya fullName
+// for konvensional
+// for of
+// filter
+
+// filter, user yang punya post > 2 post
+// for konvensional
+// for of
+// filter
